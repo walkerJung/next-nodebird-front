fc75e5bcdb2d3705c7ceb492829853d63bc03c1d
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-05-26 22:56:24 +0900

    # _app.js 및 기본 레이아웃 추가

---
+++ pages/profile.js
@@ -0,0 +1,15 @@
+import AppLayout from "../components/AppLayout";
+import Head from "next/head";
+
+const Profile = () => {
+  return (
+    <>
+      <Head>
+        <title>NodeBird | 프로필</title>
+      </Head>
+      <AppLayout>프로필</AppLayout>
+    </>
+  );
+};
+
+export default Profile;
---
+++ pages/signup.js
@@ -0,0 +1,15 @@
+import AppLayout from "../components/AppLayout";
+import Head from "next/head";
+
+const Signup = () => {
+  return (
+    <>
+      <Head>
+        <title>NodeBird | 회원가입</title>
+      </Head>
+      <AppLayout>회원가입</AppLayout>
+    </>
+  );
+};
+
+export default Signup;
