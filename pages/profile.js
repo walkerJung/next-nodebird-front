71dc20ec3dd9f9d51a74d9e91fc2074bbadceb31
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-06-01 17:58:12 +0900

    # 프로필 페이지 및 컴포넌트 추가

--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,29 @@
-import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followerList = [
+    { nickname: "WalkerJung" },
+    { nickname: "걷는사람" },
+    { nickname: "쨱쨱" },
+  ];
+  const followingList = [
+    { nickname: "WalkerJung" },
+    { nickname: "걷는사람" },
+    { nickname: "쨱쨱" },
+  ];
   return (
     <>
       <Head>
         <title>NodeBird | 프로필</title>
       </Head>
-      <AppLayout>프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
