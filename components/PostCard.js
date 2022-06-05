5fad1553f56af7a71a4f0751bb1b72f0c0bc932d
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-06-05 16:58:31 +0900

    # 댓글 구현

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Avatar, Button, Card, Popover } from "antd";
+import { Avatar, Button, Card, List, Popover, Comment } from "antd";
 import {
   EllipsisOutlined,
   HeartOutlined,
@@ -10,6 +10,7 @@ import {
 import { useSelector } from "react-redux";
 import PostImages from "./PostImages";
 import { useCallback, useState } from "react";
+import CommentForm from "./CommentForm";
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -62,7 +63,25 @@ const PostCard = ({ post }) => {
           description={post.content}
         />
       </Card>
-      {commentFormOpened && <div>댓글부분</div>}
+      {commentFormOpened && (
+        <div>
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
+        </div>
+      )}
     </div>
   );
 };
