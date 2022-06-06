c9eef73db4c600ae0029bf76a2e3b7bcfd532afb
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-06-06 13:08:51 +0900

    # hashtag 링크 구현

--- components/PostCard.js
+++ components/PostCard.js
@@ -11,6 +11,7 @@ import { useSelector } from "react-redux";
 import PostImages from "./PostImages";
 import { useCallback, useState } from "react";
 import CommentForm from "./CommentForm";
+import PostCardContent from "./PostCardContent";
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -60,7 +61,7 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpened && (
