ce87616f1d6a20cc44ffb0555aac4bd5b32d0d57
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-06-30 22:51:28 +0900

    redux, redux-saga 연동

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,12 +1,13 @@
 import { Avatar, Card, Button } from "antd";
 import { useCallback } from "react";
-import { useDispatch } from "react-redux";
-import { logoutAction } from "../reducers/user";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutRequestAction } from "../reducers/user";
 
 const UserProfile = () => {
   const dispatch = useDispatch();
+  const { me, isLoggingOut } = useSelector((state) => state.user);
   const onLogOut = useCallback(() => {
-    dispatch(logoutAction());
+    dispatch(logoutRequestAction());
   }, []);
   return (
     <Card
@@ -25,8 +26,13 @@ const UserProfile = () => {
         </div>,
       ]}
     >
-      <Card.Meta avatar={<Avatar>WJ</Avatar>} title="WalkerJung" />
-      <Button onClick={onLogOut}>로그아웃</Button>
+      <Card.Meta
+        avatar={<Avatar>{me?.nickname[0]}</Avatar>}
+        title={me?.nickname}
+      />
+      <Button onClick={onLogOut} loading={isLoggingOut}>
+        로그아웃
+      </Button>
     </Card>
   );
 };
---
+++ sagas/user.js
@@ -0,0 +1,47 @@
+import { all, fork, takeLatest, put, delay } from "redux-saga/effects";
+
+function logInAPI(data) {
+  return axios.post("/api/login", data);
+}
+
+function* logIn(action) {
+  try {
+    // const result = yield call(logInAPI, action.data);
+    yield delay(1000);
+    yield put({ type: "LOG_IN_SUCCESS", data: action.data });
+  } catch (err) {
+    yield put({
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function logOutAPI() {
+  return axios.post("/api/logout");
+}
+
+function* logOut() {
+  try {
+    // const result = yield call(logOutAPI);
+    yield delay(1000);
+    yield put({ type: "LOG_OUT_SUCCESS" });
+  } catch (err) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function* watchLogIn() {
+  yield takeLatest("LOG_IN_REQUEST", logIn);
+}
+
+function* watchLogOut() {
+  yield takeLatest("LOG_OUT_REQUEST", logOut);
+}
+
+export default function* userSaga() {
+  yield all([fork(watchLogIn), fork(watchLogOut)]);
+}
