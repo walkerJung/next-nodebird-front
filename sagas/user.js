368dd3277da8ffb6422e3dc36293f7361645c6fb
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-07-14 12:04:55 +0900

    # 로그인 구현

--- sagas/user.js
+++ sagas/user.js
@@ -19,14 +19,13 @@ import {
 } from "../reducers/user";
 
 function logInAPI(data) {
-  return axios.post("/api/login", data);
+  return axios.post("/user/login", data);
 }
 
 function* logIn(action) {
   try {
-    // const result = yield call(logInAPI, action.data);
-    yield delay(1000);
-    yield put({ type: LOG_IN_SUCCESS, data: action.data });
+    const result = yield call(logInAPI, action.data);
+    yield put({ type: LOG_IN_SUCCESS, data: result.data });
   } catch (err) {
     yield put({
       type: LOG_IN_FAILURE,
@@ -53,7 +52,7 @@ function* logOut() {
 }
 
 function signUpAPI(data) {
-  return axios.post("http://localhost:3065/user", data);
+  return axios.post("/user", data);
 }
 
 function* signUp(action) {
